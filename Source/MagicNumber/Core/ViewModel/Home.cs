cb0dd26f29fc970307374d2f9742744bc0ef9230
Brad Buhrkuhl <bbuhrkuhl@lundeng.com>
2017-01-02 13:30:19 -0800

    Finish UI plumbing

--- Source/MagicNumber/Core/ViewModel/Home.cs
+++ Source/MagicNumber/Core/ViewModel/Home.cs
@@ -8,5 +8,19 @@ namespace MagicNumber.Core.ViewModel
 {
     public class Home : Utilis.UI.ViewModel.Base
     {
+        private readonly Model.Server m_server;
+
+        public Home ( Model.Server server )
+        {
+            m_server = server;
+        }
+    }
+
+    public class HomeDesign : Home
+    {
+        public HomeDesign ( ) : base ( new Model.Server ( "magicNumber.blorq.com:15613" ) )
+        {
+
+        }
     }
 }
