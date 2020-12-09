071d6860de045d28f56612685d8d53d90c400be2
Brad Buhrkuhl <brad@blorq.com>
2020-12-08 16:12:44 -0800

    Set size of window for adding set

--- Source/MagicNumber/Windows/Main.xaml.cs
+++ Source/MagicNumber/Windows/Main.xaml.cs
@@ -18,15 +18,19 @@ namespace MagicNumber.Windows
 
         private void MainFrame_Navigated ( object sender, System.Windows.Navigation.NavigationEventArgs e )
         {
-            Pages.Home home = MainFrame.Content as Pages.Home;
-            if (home != null)
+            if ( MainFrame.Content is MagicNumber.Pages.Home home )
             {
-                if (home.ViewModel.Sets.Count > 0)
+                if ( home.ViewModel.Sets.Count > 0 )
                 {
                     this.Width = home.ViewModel.Sets.Count * 146 + 20;
                     this.Height = 225;
                     //this.Height = 200;
                 }
+            }
+            else if ( MainFrame.Content is MagicNumber.Pages.AddSet addSet )
+            {
+                this.Width = 400;
+                this.Height = 300;
             }
         }
     }
